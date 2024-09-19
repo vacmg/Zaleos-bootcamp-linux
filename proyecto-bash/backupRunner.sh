5f68a2c879ca8c19756ca0aab610cd8b24344959
vacmg <70143287+vacmg@users.noreply.github.com>
2024-09-19 10:15:32 +0200

    add mock backupRunner script to test backupConfig

--- proyecto-bash/backupRunner.sh
+++ proyecto-bash/backupRunner.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
 
+# Usage: ./backupRunner.sh backupDir backupFile
+
+if [ $# -ne 2 ]
+then
+    echo "Usage: ./backupRunner.sh backupDir backupFile" >&2
+    exit 1
+fi
+
+backupDir=$1
+backupFile=$2
+
+echo "date: $(date); Backup directory: $backupDir; Backup file: $backupFile" > "$backupFile"
